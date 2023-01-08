4a50af461d2dcd862813102d267e502c3cf144f0
WalkinBurger <nchunghim@gmail.com>
2023-01-08 15:38:29 +0000

    maybe should work

--- src/arm/testNewItem.java
+++ src/arm/testNewItem.java
@@ -2,7 +2,6 @@ package arm;
 
 import mindustry.content.Items;
 import mindustry.type.Category;
-import mindustry.type.Item;
 import mindustry.type.ItemStack;
 import mindustry.world.Block;
 public class testNewItem {
@@ -11,13 +10,9 @@ public class testNewItem {
 
     public static void load() {
         iHateMyLife = new Block("grass-water") {{
-            requirements(Category.defense, with(Items.copper, 1));
+            requirements(Category.defense, ItemStack.with(Items.copper, 1), true);
             health = 2;
             size = 1;
-        }
-
-        private ItemStack[] with(Item copper, int i) {
-            return null;
         }};
-    }
+    };
 }; 
