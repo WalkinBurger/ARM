5b2581a409d7d60197f0e09e28615a0fa5e7c7f8
WalkinBurger <nchunghim@gmail.com>
2023-01-08 22:58:43 +0000

    cssscsca

--- src/arm/testNewItem.java
+++ src/arm/testNewItem.java
@@ -4,3 +4,15 @@ import mindustry.content.Items;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.world.Block;
+public class testNewItem {
+
+    public static Block iHateMyLife;
+
+    public static void load() {
+        iHateMyLife = new Block("iHateMyLife") {{
+            requirements(Category.defense, ItemStack.with(Items.copper, 1), true);
+            health = 2;
+            size = 1;
+        }};
+    };
+}; 
