462297ff6acf277a77086c8b05614d8dce4fed61
WalkinBurger <nchunghim@gmail.com>
2023-01-08 23:37:39 +0000

    wd

--- src/arm/testNewItem.java
+++ src/arm/testNewItem.java
@@ -4,12 +4,14 @@ import mindustry.content.Items;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.world.Block;
+import mindustry.world.blocks.defense.Wall;
 public class testNewItem {
 
     public static Block iHateMyLife;
 
     public static void load() {
-        iHateMyLife = new Block("iHateMyLife") {{
+        iHateMyLife = new Wall("iHateMyLife") {{
+            requirements(Category.defense, ItemStack.with(Items.copper, 1), true);
             health = 2;
             size = 1;
         }};
