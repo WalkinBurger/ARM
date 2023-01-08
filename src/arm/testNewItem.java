4966a0547250c7e13a4049aba4601098bce16091
WalkinBurger <nchunghim@gmail.com>
2023-01-08 22:55:50 +0000

    sssss

--- src/arm/testNewItem.java
+++ src/arm/testNewItem.java
@@ -4,15 +4,3 @@ import mindustry.content.Items;
 import mindustry.type.Category;
 import mindustry.type.ItemStack;
 import mindustry.world.Block;
-public class testNewItem {
-
-    public static Block iHateMyLife;
-
-    public static void load() {
-        iHateMyLife = new Block("dumb-shit") {{
-            requirements(Category.defense, ItemStack.with(Items.copper, 1), true);
-            health = 2;
-            size = 1;
-        }};
-    };
-}; 
